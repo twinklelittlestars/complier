2d41e6728995aff1f55c7bd7bd591eaa9aa62067
twinklelittlestars <cmty_0916@qq.com>
2023-11-05 22:21:27 +0800

    update

--- NFA2DFA.cpp
+++ NFA2DFA.cpp
@@ -21,16 +21,22 @@ void minimizeDFA(edge b[], int N, string &NODE, string &CHANGE, string &endnode,
     m = 2;
     sta.erase();
     flag = 0;
+    // 划分终态和非终态
     for (i = 0; i < m; i++) {
+        // 判断是否为终态
         for (k = 0; k < len; k++) {
             y = m;
+            // 遍历当前状态的所有NFA状态
             for (j = 0; j < d[i].length(); j++) {
+                // 遍历所有DFA状态
                 for (n = 0; n < y; n++) {
+                    // 如果当前DFA状态包含NFA的终态，则将其标记为DFA的终态
                     if (d[n].find(t[NODE.find(d[i][j])].sets[k]) < d[n].length() || t[NODE.find(d[i][j])].sets[k].length() == 0) {
                         x = (t[NODE.find(d[i][j])].sets[k].length() == 0) ? m : n;
+                        // 如果当前DFA状态不是终态，则将其标记为非终态
                         if (sta.empty()) {
                             sta += x + '0';
-                        } else if (sta[0] != x + '0') {
+                        } else if (sta[0] != x + '0') {// 如果当前DFA状态不是终态，则将其标记为非终态
                             d[m] += d[i][j];
                             flag = 1;
                             d[i].erase(j, 1);
@@ -58,14 +64,20 @@ void minimizeDFA(edge b[], int N, string &NODE, string &CHANGE, string &endnode,
         NODE += md[i].state_table;
     }
 
+    // 更新状态转换矩阵
     for (i = 0; i < m; i++) {
+        // 遍历当前状态的所有NFA状态
         for (k = 0; k < len; k++) {
+            // 遍历所有DFA状态
             for (j = 0; j < h; j++) {
+                // 如果当前DFA状态包含NFA的终态，则将其标记为DFA的终态
                 if (d[i][0] == t[j].state_table[0]) {
+                    // 遍历所有DFA状态
                     for (n = 0; n < m; n++) {
+                        // 如果当前DFA状态包含NFA的终态，则将其标记为DFA的终态
                         if (t[j].sets[k].empty()) {
                             break;
-                        } else if (d[n].find(t[j].sets[k]) < d[n].length()) {
+                        } else if (d[n].find(t[j].sets[k]) < d[n].length()) {// 如果当前DFA状态不是终态，则将其标记为非终态
                             md[i].sets[k] = md[n].state_table;
                             break;
                         }
@@ -103,6 +115,7 @@ void cout_tab(int a) {
 void Sort(string &a) {
     int i, j;
     char b;
+    // 冒泡排序
     for (j = 0; j < a.length(); j++)
         for (i = 0; i < a.length(); i++)
             if (nodes_sets.find(a[i]) > nodes_sets.find(a[i + 1])) {
@@ -112,8 +125,10 @@ void Sort(string &a) {
             }
 }
 
+// 求e-clouse
 void eclouse(char c, string &he, edge b[]) {
     int k;
+    // 遍历所有边
     for (k = 0; k < edgenum; k++) {
         if (c == b[k].start[0])
             if (b[k].condition == "*") {
@@ -124,24 +139,29 @@ void eclouse(char c, string &he, edge b[]) {
     }
 }
 
+// 求move(I,a)
 void update(State_change &he, int m, edge b[]) {//
     int i, j, k, l;
     k = he.state_table.length();
     l = he.sets[m].length();
+    // 遍历所有边
     for (i = 0; i < k; i++)
         for (j = 0; j < edgenum; j++)
+        // 如果当前边的起点和条件与当前状态相同，则将其终点加入到当前状态的集合中
             if ((final_sets[m] == b[j].condition[0]) && (he.state_table[i] == b[j].start[0]))
                 if (he.sets[m].find(b[j].end[0]) > he.sets[m].length())
                     he.sets[m] += b[j].end[0];
 
+    // 遍历所有边
     for (i = 0; i < l; i++)
         for (j = 0; j < edgenum; j++)
+        // 如果当前边的起点和条件与当前状态相同，则将其终点加入到当前状态的集合中
             if ((final_sets[m] == b[j].condition[0]) && (he.sets[m][i] == b[j].start[0]))
                 if (he.sets[m].find(b[j].end[0]) > he.sets[m].length())
                     he.sets[m] += b[j].end[0];
 }
 
-// 输出
+// 格式化输出
 void mycout(int len, int h, State_change *t) {
     int i, j, m;
     cout << " I       ";
@@ -151,6 +171,7 @@ void mycout(int len, int h, State_change *t) {
     for (i = 0; i < h; i++) {
         cout << ' ' << t[i].state_table;
         m = t[i].state_table.length();
+        // 遍历所有集合
         for (j = 0; j < len; j++) {
             cout_tab(8 - m);
             m = t[i].sets[j].length();
@@ -173,33 +194,40 @@ int main() {
     }
     edgenum = i;
 
+
     for (i = 0; i < edgenum; i++) {
+        // 求结点集合
         if (nodes_sets.find(b[i].start) > nodes_sets.length()) nodes_sets += b[i].start;
-        if (nodes_sets.find(b[i].end) > nodes_sets.length()) nodes_sets += b[i].end;
-        if ((final_sets.find(b[i].condition) > final_sets.length()) && (b[i].condition != "e")) final_sets += b[i].condition;
+        if (nodes_sets.find(b[i].end) > nodes_sets.length()) nodes_sets += b[i].end;// 求终结符集合
+        if ((final_sets.find(b[i].condition) > final_sets.length()) && (b[i].condition != "*")) final_sets += b[i].condition;
     }
     len = final_sets.length();
 
     cout << "结点中属于终态的是: " << endl;
     cin >> endnode;
 
+    // 求状态转换矩阵
     State_change *t = new State_change[MAXS_states];
     t[0].state_table = b[0].start;
     h = 1;
     eclouse(b[0].start[0], t[0].state_table, b); // 求e-clouse
 
+    // 求move(I,a)
     for (i = 0; i < h; i++) {
+        // 遍历所有终结符
         for (j = 0; j < t[i].state_table.length(); j++) {
             for (m = 0; m < len; m++) {
                 eclouse(t[i].state_table[j], t[i].sets[m], b); // 求e-clouse
             }
         }
+        // 遍历所有终结符
         for (k = 0; k < len; k++) {
             update(t[i], k, b); // 求move(I,a)
             for (j = 0; j < t[i].sets[k].length(); j++) {
                 eclouse(t[i].sets[k][j], t[i].sets[k], b); // 求e-clouse
             }
         }
+        // 遍历所有集合
         for (j = 0; j < len; j++) {
             Sort(t[i].sets[j]); // 对集合排序以便比较
             for (k = 0; k < h; k++) {
@@ -219,14 +247,15 @@ int main() {
     nodes_sets.erase();
     cout << endl << "重命名: " << endl;
     for (i = 0; i < h; i++) {
-        sta = t[i].state_table;
+        sta = t[i].state_table;//当前DFA状态对应的NFA状态集合
         t[i].state_table.erase();
         t[i].state_table = 'A' + i;
         nodes_sets += t[i].state_table;
         cout << '{' << sta << "}=" << t[i].state_table << endl;
         for (j = 0; j < endnode.length(); j++) {
+            // 如果当前DFA状态包含NFA的终态，则将其标记为DFA的终态
             if (sta.find(endnode[j]) < sta.length()) {
-                d[i] = t[i].state_table;
+                d[i] = t[i].state_table;// d数组用于存储DFA的终态
                 break;
             }
         }
@@ -248,16 +277,15 @@ int main() {
     }
     cout << endl;
 
-
-    // Prompt the user if they want to minimize the DFA
+    // DFA最小化
     string userChoice;
     cout << "是否要进行DFA最小化? (Yes/No): ";
     cin >> userChoice;
 
-    // Based on user choice, either show the minimized DFA or the original DFA
+    // 执行DFA最小化
     if (userChoice == "Yes" || userChoice == "yes") {
         cout << "最小化DFA如下: " << endl;
-        mycout(len, h, t); // Output the minimized DFA
+        mycout(len, h, t); 
         cout << "其中终态为: " << endnode << endl;
     } else {
         cout << "未执行DFA最小化。" << endl;
